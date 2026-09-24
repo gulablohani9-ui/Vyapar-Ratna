814005c8b63cc4ee219cbd6221a0513d5461b710
gulablohani9-ui <gulablohani9@gmail.com>
2026-09-24 23:56:33 +0530

    Update MainActivity.kt

--- app/src/main/java/com/vyaparratna/MainActivity.kt
+++ app/src/main/java/com/vyaparratna/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -56,7 +57,7 @@ fun HomeScreen() {
             items(menuItems) { item ->
                 Card(
                     modifier = Modifier.fillMaxWidth(),
-                    onClick = { }
+                    onClick = { /* Baad me navigation add karenge */ }
                 ) {
                     Text(
                         text = item,
