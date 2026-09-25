17832dc162901632457cb7c06532a3a9b08078f5
gulablohani9-ui <gulablohani9@gmail.com>
2026-09-25 05:53:33 +0530

    Create NavGraph.kt

---
+++ app/src/main/java/com/vyaparratna/NavGraph.kt
@@ -0,0 +1,21 @@
+package com.vyaparratna
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun NavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") {
+            HomeScreen(navController)
+        }
+        composable("dhruvank") {
+            DhruvankScreen(navController)
+        }
+    }
+}
