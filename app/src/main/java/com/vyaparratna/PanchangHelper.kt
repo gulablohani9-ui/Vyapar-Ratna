844fd85c3f909a0f7cac65bef7e019b8ac7ca122
gulablohani9-ui <gulablohani9@gmail.com>
2026-09-25 12:55:32 +0530

    Add approximate tithi and nakshatra calculations

--- app/src/main/java/com/vyaparratna/PanchangHelper.kt
+++ app/src/main/java/com/vyaparratna/PanchangHelper.kt
@@ -3,11 +3,27 @@ package com.vyaparratna
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
+import java.util.GregorianCalendar
 import java.util.Locale
 
 object PanchangHelper {
 
-    // Date se Vaar nikalo (100% accurate)
+    private val nakshatraList = listOf(
+        "अश्विनी", "भरणी", "कृत्तिका", "रोहिणी", "मृगशिरा",
+        "आर्द्रा", "पुनर्वसु", "पुष्य", "अश्लेषा", "मघा",
+        "पूर्वाफाल्गुनी", "उत्तराफाल्गुनी", "हस्त", "चित्रा",
+        "स्वाति", "विशाखा", "अनुराधा", "ज्येष्ठा", "मूल",
+        "पूर्वाषाढा", "उत्तराषाढा", "श्रवण", "धनिष्ठा",
+        "शतभिषा", "पूर्वाभाद्रपद", "उत्तराभाद्रपद", "रेवती"
+    )
+
+    private val tithiList = listOf(
+        "प्रतिपदा", "द्वितीया", "तृतीया", "चतुर्थी", "पंचमी",
+        "षष्ठी", "सप्तमी", "अष्टमी", "नवमी", "दशमी",
+        "एकादशी", "द्वादशी", "त्रयोदशी", "चतुर्दशी"
+    )
+
+    // Date se Vaar
     fun getVaarFromDate(date: Date): String {
         val cal = Calendar.getInstance()
         cal.time = date
@@ -23,7 +39,34 @@ object PanchangHelper {
         }
     }
 
-    // Date ko format karo (Hindi mein)
+    // Approximate Tithi (based on lunar month cycle)
+    fun getApproxTithi(date: Date): String {
+        // Reference new moon: 11 January 2024
+        val refNewMoon = GregorianCalendar(2024, 0, 11, 11, 57).timeInMillis
+        val daysSince = (date.time - refNewMoon) / (1000.0 * 60 * 60 * 24)
+        val lunarMonth = 29.530588853
+        val daysInCycle = ((daysSince % lunarMonth) + lunarMonth) % lunarMonth
+        val tithiNum = (daysInCycle / (lunarMonth / 30.0)).toInt() + 1
+
+        return if (tithiNum <= 15) {
+            if (tithiNum == 15) "पूर्णिमा" else tithiList[tithiNum - 1]
+        } else {
+            val krishnaNum = tithiNum - 15
+            if (krishnaNum == 15) "अमावास्या" else tithiList[krishnaNum - 1]
+        }
+    }
+
+    // Approximate Nakshatra (based on sidereal month cycle)
+    fun getApproxNakshatra(date: Date): String {
+        // Reference: 1 January 2024, Moon in approximate position
+        val refDate = GregorianCalendar(2024, 0, 1).timeInMillis
+        val daysSince = (date.time - refDate) / (1000.0 * 60 * 60 * 24)
+        val nakshatraLength = 27.321661 / 27.0 // ~1.012 days per nakshatra
+        val idx = (((daysSince / nakshatraLength).toInt() % 27) + 27) % 27
+        return nakshatraList[idx]
+    }
+
+    // Date formatting
     fun formatDateHindi(date: Date): String {
         val cal = Calendar.getInstance()
         cal.time = date
@@ -47,7 +90,7 @@ object PanchangHelper {
         return "$day $month $year"
     }
 
-    // Aaj, kal, parson ki date
+    // Aaj, kal, parson
     fun today(): Date = Date()
 
     fun tomorrow(): Date {
@@ -59,13 +102,6 @@ object PanchangHelper {
     fun dayAfterTomorrow(): Date {
         val cal = Calendar.getInstance()
         cal.add(Calendar.DAY_OF_YEAR, 2)
-        return cal.time
-    }
-
-    // Custom date
-    fun customDate(year: Int, month: Int, day: Int): Date {
-        val cal = Calendar.getInstance()
-        cal.set(year, month, day)
         return cal.time
     }
 
